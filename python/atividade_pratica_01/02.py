3e1649a356c2928534a489a2b88cadf5c166df42
Italo José <italoc.silva21@gmail.com>
2025-10-28 19:47:27 -0300

    Atividade pratica 1 completa

---
+++ python/atividade_pratica_01/02.py
@@ -0,0 +1,7 @@
+#Desenvolva um programa que soma dois números.
+#Use as variáveis numero1 = 12 e numero2 = 14. 
+
+numero1 = 12
+numero2 = 14
+soma = numero1 + numero2
+print("A soma de", numero1, "e", numero2, "e igual a", soma)
---
+++ python/atividade_pratica_01/04.py
@@ -0,0 +1,11 @@
+"""Desenvolva um programa que calcule o preço total de uma compra. Use as seguintes informações:
+
+Nome do produto: "Cadeira Infantil"
+Preço unitário: R$ 12.40
+Quantidade: 3"""
+
+nome_produto = "Cadeira Infantil"
+preco_unitario = 12.40
+quantidade = 3
+preco_total = preco_unitario * quantidade
+print("O preco total da compra de", quantidade, nome_produto + " e R$", preco_total)
